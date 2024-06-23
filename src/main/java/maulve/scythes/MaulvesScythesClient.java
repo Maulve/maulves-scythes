ebbacd4495d13db3c526cd0faadadc0971187fed
Maulve <aether2571@gmail.com>
2024-06-23 16:46:28 +0200

    Initial commit

---
+++ src/main/java/maulve/scythes/MaulvesScythesClient.java
@@ -0,0 +1,11 @@
+package maulve.scythes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MaulvesScythesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
