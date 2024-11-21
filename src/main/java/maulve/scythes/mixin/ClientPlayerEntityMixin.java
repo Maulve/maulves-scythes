546bbeebe1385e5604f8764c406fb023e24ccdff
Maulve <aether2571@gmail.com>
2024-11-21 22:16:59 +0100

    apply Bleeding to sweeping targets

--- src/main/java/maulve/scythes/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/maulve/scythes/mixin/ClientPlayerEntityMixin.java
@@ -1,13 +1,22 @@
 package maulve.scythes.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import maulve.scythes.item.AmethystScytheItem;
 import maulve.scythes.item.ScytheItem;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = PlayerEntity.class, priority = 800)
 public abstract class ClientPlayerEntityMixin {
@@ -20,6 +29,15 @@ public abstract class ClientPlayerEntityMixin {
         return heldItem instanceof ScytheItem;
     }
 
+    @Unique
+    private boolean holdingAmethystScythe() {
+        PlayerEntity player = MinecraftClient.getInstance().player;
+        assert player != null;
+        Item heldItem = player.getMainHandStack().getItem();
+
+        return heldItem instanceof AmethystScytheItem;
+    }
+
     // modifies doSweepAttack (bl4) if player is holding a Scythe
     @ModifyVariable(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getHealth()F"), ordinal = 3)
     private boolean overrideBL4(boolean bl4) {
@@ -27,5 +45,16 @@ public abstract class ClientPlayerEntityMixin {
             return true;
         }
         return bl4;
+    }
+
+    // adds Bleeding to sweeping attack targets
+    @Inject(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
+    private void addBleeding(Entity target, CallbackInfo ci, @Local(ordinal = 0) @NotNull LivingEntity livingEntity) {
+        if (holdingAmethystScythe()) {
+            StatusEffectInstance effectWither = new StatusEffectInstance(StatusEffects.WITHER, 80, 1);
+            StatusEffectInstance effectSlowness = new StatusEffectInstance(StatusEffects.SLOWNESS, 20, 1);
+            livingEntity.addStatusEffect(effectWither);
+            livingEntity.addStatusEffect(effectSlowness);
+        }
     }
 }
--- src/main/java/maulve/scythes/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/maulve/scythes/mixin/ServerPlayerEntityMixin.java
@@ -1,12 +1,21 @@
 package maulve.scythes.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import maulve.scythes.item.AmethystScytheItem;
 import maulve.scythes.item.ScytheItem;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = PlayerEntity.class, priority = 800)
 public abstract class ServerPlayerEntityMixin {
@@ -18,6 +27,14 @@ public abstract class ServerPlayerEntityMixin {
         return heldItem instanceof ScytheItem;
     }
 
+    @Unique
+    private boolean holdingAmethystScythe() {
+        PlayerEntity player = (PlayerEntity)(Object) this;
+        Item heldItem = player.getMainHandStack().getItem();
+
+        return heldItem instanceof AmethystScytheItem;
+    }
+
     // modifies doSweepAttack (bl4) if player is holding a Scythe
     @ModifyVariable(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getHealth()F"), ordinal = 3)
     private boolean overrideBL4(boolean bl4) {
@@ -25,5 +42,16 @@ public abstract class ServerPlayerEntityMixin {
             return true;
         }
         return bl4;
+    }
+
+    // adds Bleeding to sweeping attack targets
+    @Inject(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
+    private void addBleeding(Entity target, CallbackInfo ci, @Local(ordinal = 0) @NotNull LivingEntity livingEntity) {
+        if (holdingAmethystScythe()) {
+            StatusEffectInstance effectWither = new StatusEffectInstance(StatusEffects.WITHER, 80, 1);
+            StatusEffectInstance effectSlowness = new StatusEffectInstance(StatusEffects.SLOWNESS, 20, 1);
+            livingEntity.addStatusEffect(effectWither);
+            livingEntity.addStatusEffect(effectSlowness);
+        }
     }
 }
