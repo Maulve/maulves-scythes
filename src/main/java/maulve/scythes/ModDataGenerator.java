44b3c02cab44c11f92d170ea713aa73de54ac855
Maulve <aether2571@gmail.com>
2024-12-08 22:00:46 +0100

    fix models

--- src/main/java/maulve/scythes/ModDataGenerator.java
+++ src/main/java/maulve/scythes/ModDataGenerator.java
@@ -2,26 +2,21 @@ package maulve.scythes;
 
 import maulve.scythes.datagen.*;
 import maulve.scythes.enchantment.ModEnchantments;
-import net.fabricmc.api.EnvType;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.registry.RegistryBuilder;
 import net.minecraft.registry.RegistryKeys;
 
 public class ModDataGenerator implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+        final FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
         pack.addProvider(ModRecipeProvider::new);
+        pack.addProvider(ModModelProvider::new);
         pack.addProvider(ModRegistryDataGenerator::new);
         pack.addProvider(ModItemTagProvider::new);
         pack.addProvider(ModAdvancementProvider::new);
-
-        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
-            pack.addProvider(ModModelProvider::new);
-        }
 
     }
 
--- src/main/java/maulve/scythes/datagen/ModModelProvider.java
+++ src/main/java/maulve/scythes/datagen/ModModelProvider.java
@@ -1,16 +1,12 @@
 package maulve.scythes.datagen;
 
 import maulve.scythes.item.ModItems;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.client.data.BlockStateModelGenerator;
 import net.minecraft.client.data.ItemModelGenerator;
 import net.minecraft.client.data.Models;
 
-//TODO: fix this
-@Environment(EnvType.CLIENT)
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
         super(output);
