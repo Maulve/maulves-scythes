469144cb89b729ae2ec91e291d1f15ca5779b9dc
Maulve <aether2571@gmail.com>
2024-12-08 14:10:18 +0100

    fix item group

--- src/main/java/maulve/scythes/item/ModItemGroups.java
+++ src/main/java/maulve/scythes/item/ModItemGroups.java
@@ -1,10 +1,7 @@
 package maulve.scythes.item;
 
 import maulve.scythes.MaulvesScythes;
-import maulve.scythes.enchantment.ModEnchantments;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.minecraft.enchantment.EnchantmentLevelEntry;
-import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
@@ -15,7 +12,7 @@ import net.minecraft.util.Identifier;
 public class ModItemGroups {
     public static final ItemGroup SCYTHE_GROUP = Registry.register(Registries.ITEM_GROUP,
             new Identifier(MaulvesScythes.MOD_ID, "scythe"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.scythe"))
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.maulves-scythes.scythes"))
                     .icon(() -> new ItemStack(ModItems.DIAMOND_SCYTHE)).entries((displayContext, entries) -> {
                         entries.add(ModItems.IRON_SCYTHE);
                         entries.add(ModItems.DIAMOND_SCYTHE);
@@ -23,10 +20,6 @@ public class ModItemGroups {
                         entries.add(ModItems.AMETHYST_SCYTHE);
                         entries.add(ModItems.REINFORCED_STICK);
                         entries.add(ModItems.SCYTHED_AMETHYST);
-                        entries.add(EnchantedBookItem.forEnchantment(new EnchantmentLevelEntry(ModEnchantments.LIFE_DRAIN, 1)));
-                        entries.add(EnchantedBookItem.forEnchantment(new EnchantmentLevelEntry(ModEnchantments.LIFE_DRAIN, 2)));
-                        entries.add(EnchantedBookItem.forEnchantment(new EnchantmentLevelEntry(ModEnchantments.LIFE_DRAIN, 3)));
-                        entries.add(EnchantedBookItem.forEnchantment(new EnchantmentLevelEntry(ModEnchantments.SWEEPING_ARC, 1)));
                     }).build());
 
     public static void registerItemGroups() {
